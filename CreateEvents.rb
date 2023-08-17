f023c3ff606e3c3f641ec8c3606e4ef3d49c31a9
Blag <atejada@gmail.com>
2023-08-17 06:24:53 -0400

    Update CreateEvents.rb

--- CreateEvents.rb
+++ CreateEvents.rb
@@ -1,49 +1,31 @@
-# Load gems
 require 'dotenv/load'
 require 'nylas'
+require 'date'
 
-# Initialize Nylas client
-nylas = Nylas::Client.new(
-	api_key: ENV["V3_TOKEN"],
-	host: ENV["V3_HOST"]
+nylas = Nylas::API.new(
+    app_id: ENV["CLIENT_ID"],
+    app_secret: ENV["CLIENT_SECRET"],
+    access_token: ENV["ACCESS_TOKEN"]
 )
 
-# Identifier
-identifier = {
-    grant_id: ENV["GRANT_ID"]
-}
-
-# Query parameters
-query_params = {
-    calendar_id: ENV["GRANT_ID"]
-}
-
-# Get today’s date
 today = Date.today
-# Define the time of the event
-start_time = Time.local(today.year, today.month, today.day, 10, 0,0).strftime("%s")
-end_time = Time.local(today.year, today.month, today.day, 12, 0,0).strftime("%s")
 
-# Request Body
-request_body = {
-	when: {
-		start_time: start_time.to_i,
-		end_time: end_time.to_i
-	},
-	title: "Let's learn some Nylas Ruby SDK!",
-	location: "Blag's Den!",
-	description: "Using the Nylas API with the Ruby SDK is easy. Come join us!\"",
-	participants: [{
-        name: "Blag",
-        email: "xxx@xxx.com", 
-        status: 'noreply'
-      }]
+test = ["atejada@gmail.com","alvaro.t@nylas.com"]
+emails = []
+
+test.each { |event|
+	emails.push({email: "#{event}"})
 }
 
-# Read events from our main calendar in the specified date and time
-events, _request_ids = nylas.events.create(identifier: ENV["GRANT_ID"], query_params: query_params, request_body: request_body)
-if _request_ids != "" 
-	puts "Event created successfully"
+event = nylas.events.create(title: "Let's learn some Nylas Ruby API!", location: "Blag's Den!", 
+							when: { start_time: Time.local(today.year, today.month, today.day, 13, 0,0).strftime("%s"), 
+							end_time: Time.local(today.year, today.month, today.day, 14, 0,0).strftime("%s") },
+							participants: emails,
+							calendar_id: "d7o9zmx9f2yh6dxonxfrp8crb",
+							notify_participants: true)
+
+if event.id
+	puts 'Event created successfully'
 else
-   puts "There was an error creating the event"
+	puts 'There was an error creating the event'
 end
